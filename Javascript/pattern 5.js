0c39206e6c8c34e8f8ff02cd94f0567e725c55dc
ejini战神 <65498235+ejini6969@users.noreply.github.com>
2021-06-02 15:22:29 +0800

    Update pattern 5.js

--- Javascript/pattern 5.js
+++ Javascript/pattern 5.js
@@ -4,3 +4,22 @@ function pattern(n){
 
 const { assert } = require("chai")
 
+describe("Tests", () => {
+  it("test", () => {
+  	 assert.strictEqual(pattern(2),"22");
+  	 assert.strictEqual(pattern(1),"");
+   	assert.strictEqual(pattern(5),"22\n4444");
+    assert.strictEqual(pattern(0),"");
+    assert.strictEqual(pattern(-25),"");
+    assert.strictEqual(pattern(8),"22\n4444\n666666\n88888888");
+  });
+});
+
+⬇️⬇️
+
+/* Example:
+22
+4444
+666666
+88888888
+*/
