2cf1949607ae0100e8ecd3018ce2af09aa73530b
ejini战神 <65498235+ejini6969@users.noreply.github.com>
2021-06-21 17:46:26 +0800

    Create Custom Christmas Tree.js

---
+++ Javascript/Custom Christmas Tree.js
@@ -0,0 +1,49 @@
+function customChristmasTree(chars,n){
+  chars = chars.padEnd(n * (n + 1) / 2, chars)
+  let tree = Array.from({length: n}, (_, i) => `${" ".repeat(n - i - 1)}${chars.split("").slice(i * (i + 1) / 2 | 0, (i + 1) * (i + 2) / 2 | 0).join(" ")}`).join`\n` 
+  let trunk = `${" ".repeat(n - 1)}|\n`.repeat((n / 3) | 0).replace(/\n$/, "")
+  return tree + "\n" + trunk
+}
+
+const { assert } = require("chai")
+
+describe("Tests", () => {
+  it("test", () => {
+	assert.strictEqual(customChristmasTree("*@o",3),
+                                          `  *
+                                           @ o
+                                          * @ o
+                                            |`));
+	assert.strictEqual(customChristmasTree("*@o",6),
+                                        `     *
+                                            @ o
+                                           * @ o
+                                          * @ o *
+                                         @ o * @ o
+                                        * @ o * @ o
+                                             |
+                                             |`);
+	assert.strictEqual(customChristmasTree("1234",6),
+                                        `     1
+                                            2 3
+                                           4 1 2
+                                          3 4 1 2
+                                         3 4 1 2 3
+                                        4 1 2 3 4 1
+                                             |
+                                             |`));
+  });
+});
+
+⬇️⬇️
+
+/* Example:
+     *
+    @ o
+   * @ o
+  * @ o *
+ @ o * @ o
+* @ o * @ o
+     |
+     |
+*/
