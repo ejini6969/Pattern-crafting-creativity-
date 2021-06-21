b67e2b236f5d93eb6fbe0dd6dafa180873b470fb
ejini战神 <65498235+ejini6969@users.noreply.github.com>
2021-06-21 17:41:30 +0800

    Create Christmas tree.js

---
+++ Javascript/Christmas tree.js
@@ -0,0 +1,24 @@
+function christmasTree(height) {
+  return Array.from({length : height}, (_, i) => ' '.repeat(height - i - 1) + '*'.repeat(i * 2 + 1) + ' '.repeat(height - i - 1)).join("\n")
+}
+
+const { assert } = require("chai")
+
+describe("Tests", () => {
+  it("test", () => {
+    assert.strictEqual(christmasTree(0)), "");
+    assert.strictEqual(christmasTree(1), "*");
+    assert.strictEqual(christmasTree(5)), "    *    \n   ***   \n  *****  \n ******* \n*********");
+    assert.strictEqual(christmasTree(8)), "       *       \n      ***      \n     *****     \n    *******    \n   *********   \n  ***********  \n ************* \n***************");
+  });
+});
+
+⬇️⬇️
+
+/* Example:
+    *    
+   ***   
+  *****  
+ ******* 
+*********
+*/
