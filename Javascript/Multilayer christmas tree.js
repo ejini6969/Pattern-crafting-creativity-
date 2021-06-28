568f0f019698ddc8869f5d72028c763d6f620fd0
ejini战神 <65498235+ejini6969@users.noreply.github.com>
2021-06-28 17:05:49 +0800

    Create Multilayer christmas tree.js

---
+++ Javascript/Multilayer christmas tree.js
@@ -0,0 +1,40 @@
+function christmasTree(height){
+  if(height < 3) return ""
+  const max = 5 + 2 * (~~(height / 3) - 1)
+  return Array.from({length: ~~(height / 3)}, function(_, i){
+    const l = i * 2 + 1
+    return Array.from({length: 3}, (_, j) => " ".repeat((max - 1) / 2 - j - i) + "*".repeat(j * 2 + l)).join("\r\n")
+  }).join("\r\n")  + `\r\n${" ".repeat((max - 3) / 2)}###`
+}
+
+const { assert } = require("chai")
+
+describe("Tests", () => {
+  it("test", () => {
+    assert.strictEqual(christmasTree(5),"  *\r\n ***\r\n*****\r\n ###");
+    assert.strictEqual(christmasTree(10),"    *\r\n   ***\r\n  *****\r\n   ***\r\n  *****\r\n *******\r\n  *****\r\n *******\r\n*********\r\n   ###");
+    assert.strictEqual(christmasTree(8),"   *\r\n  ***\r\n *****\r\n  ***\r\n *****\r\n*******\r\n  ###");
+    assert.strictEqual(christmasTree(2),"");
+  });
+});
+
+⬇️⬇️
+
+/* Example(17):
+      *
+     ***
+    *****
+     ***
+    *****
+   *******
+    *****
+   *******
+  *********
+   *******
+  *********
+ ***********
+  *********
+ ***********
+*************
+     ###     
+*/
