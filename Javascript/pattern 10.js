bb958b5f442abafe8e429f35340df41ab46c3384
ejini战神 <65498235+ejini6969@users.noreply.github.com>
2021-06-28 18:17:41 +0800

    Create pattern 10.js

---
+++ Javascript/pattern 10.js
@@ -0,0 +1,29 @@
+function pattern(n){
+  let a = Array.from({length: n} , (_, i) => (i + 1) % 10).join('')
+  return Array.from({length:n}, (_, i) => ' '.repeat(n - i - 1) + a + ' '.repeat(i)).join('\n')
+}
+
+const { assert } = require("chai")
+
+describe("Tests", () => {
+  it("test", () => {
+    assert.strictEqual(pattern(3),"  123\n 123 \n123  ");
+    assert.strictEqual(pattern(5),"    12345\n   12345 \n  12345  \n 12345   \n12345    ");
+    assert.strictEqual(pattern(8),"       12345678\n      12345678 \n     12345678  \n    12345678   \n   12345678    \n  12345678     \n 12345678      \n12345678       ");
+  });
+});
+
+⬇️⬇️
+
+/* Example:
+         1234567890
+        1234567890 
+       1234567890  
+      1234567890   
+     1234567890    
+    1234567890     
+   1234567890      
+  1234567890       
+ 1234567890        
+1234567890       
+*/
