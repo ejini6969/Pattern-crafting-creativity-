dbf4ccb8c61640b35c665a854f06a3dd3e4d47aa
ejini战神 <65498235+ejini6969@users.noreply.github.com>
2021-07-05 14:29:06 +0800

    Create square.js

---
+++ Javascript/square.js
@@ -0,0 +1,24 @@
+function generateShape(int){
+  return Array(int).fill('+'.repeat(int)).join('\n')
+}
+
+const { assert } = require("chai")
+
+describe("Tests", () => {
+  it("test", () => {
+	  assert.strictEqual(generateShape(8) , '++++++++\n++++++++\n++++++++\n++++++++\n++++++++\n++++++++\n++++++++\n++++++++' )
+  });
+});
+
+⬇️⬇️
+
+/* Example：
+++++++++
+++++++++
+++++++++
+++++++++
+++++++++
+++++++++
+++++++++
+++++++++
+*/
