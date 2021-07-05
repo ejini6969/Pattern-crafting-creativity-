83f4e5cbd914265ad4ac9b6af2bd198db3e8931b
ejini战神 <65498235+ejini6969@users.noreply.github.com>
2021-07-05 18:34:11 +0800

    Update stairs.js

--- Javascript/stairs.js
+++ Javascript/stairs.js
@@ -6,7 +6,7 @@ const { assert } = require("chai")
 
 describe("Tests", () => {
   it("test", () => {
-	  assert.strictEqual(stairs(3),"        1 1\n    1 2 2 1\n1 2 3 3 2 1");
+    assert.strictEqual(stairs(3),"        1 1\n    1 2 2 1\n1 2 3 3 2 1");
     assert.strictEqual(stairs(7),"                        1 1\n                    1 2 2 1\n                1 2 3 3 2 1\n            1 2 3 4 4 3 2 1\n        1 2 3 4 5 5 4 3 2 1\n    1 2 3 4 5 6 6 5 4 3 2 1\n1 2 3 4 5 6 7 7 6 5 4 3 2 1")
   });
 });
