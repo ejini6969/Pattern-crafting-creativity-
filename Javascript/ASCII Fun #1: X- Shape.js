546adbeebb02b96df2734ebc3d34218e6fe24a6b
ejini战神 <65498235+ejini6969@users.noreply.github.com>
2021-07-05 14:57:48 +0800

    Create ASCII Fun #1: X- Shape.js

---
+++ Javascript/ASCII Fun #1: X- Shape.js
@@ -0,0 +1,20 @@
+function x(n){
+  return Array.from({length : n}, (_, i) => Array.from({length : n}, (_, j) => i == j || i == n - j - 1 ? '■' : '□').join``).join("\n")
+}
+
+const { assert } = require("chai")
+
+describe("Tests", () => {
+  it("test", () => {
+    assert.strictEqual(x(3), "■□■\n□■□\n■□■");
+    assert.strictEqual(x(5), "■□□□■\n□■□■□\n□□■□□\n□■□■□\n■□□□■");
+  });
+});
+
+⬇️⬇️
+
+/* Example(3)：
+■□■
+□■□
+■□■
+*/
