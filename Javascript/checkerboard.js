9ad8571111cf14380beedba707210fa271150f17
ejini战神 <65498235+ejini6969@users.noreply.github.com>
2021-07-05 18:24:28 +0800

    Create checkerboard.js

---
+++ Javascript/checkerboard.js
@@ -0,0 +1,26 @@
+function checkerboard (size) {
+  return size <= 0 ? '' : Array.from({length : size}, (_, i) => Array.from({length : size}, (_, j) => (i + j) % 2 ? '[b]' : '[r]').join``).join('\n') + '\n'
+};
+
+const { assert } = require("chai")
+
+describe("Tests", () => {
+  it("test", () => {
+	  assert.strictEqual(checkerboard(0), "");
+    assert.strictEqual(checkerboard(5), "[r][b][r][b][r]\n[b][r][b][r][b]\n[r][b][r][b][r]\n[b][r][b][r][b]\n[r][b][r][b][r]\n");
+    assert.strictEqual(checkerboard(8), "[r][b][r][b][r][b][r][b]\n[b][r][b][r][b][r][b][r]\n[r][b][r][b][r][b][r][b]\n[b][r][b][r][b][r][b][r]\n[r][b][r][b][r][b][r][b]\n[b][r][b][r][b][r][b][r]\n[r][b][r][b][r][b][r][b]\n[b][r][b][r][b][r][b][r]\n");
+  });
+});
+
+⬇️⬇️
+
+/* Example(8)：
+[r][b][r][b][r][b][r][b]
+[b][r][b][r][b][r][b][r]
+[r][b][r][b][r][b][r][b]
+[b][r][b][r][b][r][b][r]
+[r][b][r][b][r][b][r][b]
+[b][r][b][r][b][r][b][r]
+[r][b][r][b][r][b][r][b]
+[b][r][b][r][b][r][b][r]
+*/
