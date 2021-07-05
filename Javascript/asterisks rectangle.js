a2cbf73bc56b66bd71dbb04be2626511ea53dce7
ejini战神 <65498235+ejini6969@users.noreply.github.com>
2021-07-05 15:51:06 +0800

    Create asterisks rectangle.js

---
+++ Javascript/asterisks rectangle.js
@@ -0,0 +1,25 @@
+function getRectangleString(width, height) {
+  if(width < 2 || height < 2) 
+    return `*\r\n`.repeat(height)
+  const side = `${'*'.repeat(width)}` 
+  const middle = Array.from({length: height - 2}, (_, i) => `*${' '.repeat(width - 2)}*`).join`\r\n`
+  return ([side, middle, side].join`\r\n` + "\r\n").replace(/(\r\n)\1+/g, "\r\n")
+}
+
+const { assert } = require("chai")
+
+describe("Tests", () => {
+  it("test", () => {
+	assert.strictEqual(getRectangleString(3, 3), "***\r\n" +
+                                               "* *\r\n" +
+                                               "***\r\n)
+  });
+});
+
+⬇️⬇️
+
+/* Example：
+***
+* *
+***
+*/
