bab9e4643e7442e8770a48c2e2838b547d1add77
ejini战神 <65498235+ejini6969@users.noreply.github.com>
2021-07-05 18:31:49 +0800

    Update checkerboard.js

--- Javascript/checkerboard.js
+++ Javascript/checkerboard.js
@@ -6,7 +6,7 @@ const { assert } = require("chai")
 
 describe("Tests", () => {
   it("test", () => {
-	  assert.strictEqual(checkerboard(0), "");
+    assert.strictEqual(checkerboard(0), "");
     assert.strictEqual(checkerboard(5), "[r][b][r][b][r]\n[b][r][b][r][b]\n[r][b][r][b][r]\n[b][r][b][r][b]\n[r][b][r][b][r]\n");
     assert.strictEqual(checkerboard(8), "[r][b][r][b][r][b][r][b]\n[b][r][b][r][b][r][b][r]\n[r][b][r][b][r][b][r][b]\n[b][r][b][r][b][r][b][r]\n[r][b][r][b][r][b][r][b]\n[b][r][b][r][b][r][b][r]\n[r][b][r][b][r][b][r][b]\n[b][r][b][r][b][r][b][r]\n");
   });
