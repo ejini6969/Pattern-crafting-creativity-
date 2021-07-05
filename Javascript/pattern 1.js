0ca53f7b14b10d6fb95d96aedfb577c656c0d625
ejini战神 <65498235+ejini6969@users.noreply.github.com>
2021-07-05 18:32:17 +0800

    Update pattern 1.js

--- Javascript/pattern 1.js
+++ Javascript/pattern 1.js
@@ -6,9 +6,9 @@ const { assert } = require("chai")
 
 describe("Tests", () => {
   it("test", () => {
-	assert.strictEqual(pattern(1),"1");
-	assert.strictEqual(pattern(2),"1\n22");
-	assert.strictEqual(pattern(5),"1\n22\n333\n4444\n55555");
+    assert.strictEqual(pattern(1),"1");
+    assert.strictEqual(pattern(2),"1\n22");
+    assert.strictEqual(pattern(5),"1\n22\n333\n4444\n55555");
   });
 });
 
