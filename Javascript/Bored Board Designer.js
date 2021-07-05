3ebd663e44f3bbad5add9a75e190971a9904dc1f
ejini战神 <65498235+ejini6969@users.noreply.github.com>
2021-07-05 18:29:51 +0800

    Create Bored Board Designer.js

---
+++ Javascript/Bored Board Designer.js
@@ -0,0 +1,35 @@
+function generateBoard(cols, rows) {
+  return Array.from({length: rows}, (_, i) => Array.from({length: cols}, (_, j) => (i + j) % 2? '#': ' ').join``).join('\n') + '\n'
+}
+
+const { assert } = require("chai")
+
+describe("Tests", () => {
+  it("test", () => {
+	  assert.strictEqual(generateBoard(3,3),` # 
+                                           # #
+                                            # 
+                                            `);
+    assert.strictEqual(generateBoard(5,9), ` # # 
+                                            # # #
+                                             # # 
+                                            # # #
+                                             # # 
+                                            # # #
+                                             # #
+                                            # # #
+                                             # # 
+                                          `);
+	});
+});
+
+⬇️⬇️
+
+/* Example(6, 6)：
+ # # #
+# # # 
+ # # #
+# # #
+ # # #
+# # #
+*/
