19c59b96ee60a20d38d9f9d89714f6346e25807a
ejini战神 <65498235+ejini6969@users.noreply.github.com>
2021-07-05 14:26:30 +0800

    Create square box.js

---
+++ Javascript/square box.js
@@ -0,0 +1,16 @@
+function box(n){
+ return ['-'.repeat(n)].concat(Array.from({length: n - 2}, (_, i) => '-' + ' '.repeat(n - 2) + '-')).concat(['-'.repeat(n)])
+}
+
+const { assert } = require("chai")
+
+describe("Tests", () => {
+  it("test", () => {
+    assert.deepEqual(box(5), ["-----", "-   -", "-   -", "-   -", "-----"]);
+    assert.deepEqual(box(2), ["--", "--"]);
+    assert.deepEqual(box(4), ["----", "-  -", "-  -", "----"]);
+    assert.deepEqual(box(6), ["------", "-    -", "-    -", "-    -", "-    -", "------"]);
+    assert.deepEqual(box(3), ["---", "- -", "---"]);
+  });
+});
+
