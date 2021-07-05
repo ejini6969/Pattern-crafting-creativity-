22caa41ca233779db538b4c406d73395ab758317
ejini战神 <65498235+ejini6969@users.noreply.github.com>
2021-07-05 15:54:53 +0800

    Create frame.js

---
+++ Javascript/frame.js
@@ -0,0 +1,24 @@
+const frame = (text, char) => {
+ let l = Math.max(...text.map(x => x.length))
+ return `${char.repeat(l + 4)}\n` + text.map(x => `${char} ${x}${' '.repeat(l - x.length)} ${char}`).join('\n') + '\n' + `${char.repeat(l + 4)}`
+};
+
+const { assert } = require("chai")
+
+describe("Tests", () => {
+  it("test", () => {
+	  assert.strictEqual(frame(['Small','frame']), '~~~~~~~~~\n~ Small ~\n~ frame ~\n~~~~~~~~~');
+    assert.strictEqual(frame(['Create','this','kata']), '++++++++++\n+ Create +\n+ this   +\n+ kata   +\n++++++++++');
+    assert.strictEqual(frame(['This is a very long single frame']), '------------------------------------\n- This is a very long single frame -\n------------------------------------');
+  });
+});
+
+⬇️⬇️
+
+/* Example(['Create', 'a', 'frame'])：
+++++++++++
++ Create +
++ a      +
++ frame  +
+++++++++++
+*/
