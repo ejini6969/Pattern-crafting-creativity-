afdfa44e0701ee585e25ae68c47aa24e2ef80ac9
ejini战神 <65498235+ejini6969@users.noreply.github.com>
2021-07-05 14:51:16 +0800

    Create tower.js

---
+++ Javascript/tower.js
@@ -0,0 +1,23 @@
+function towerBuilder(nFloors) {
+  return Array.from({length: nFloors}, (_, i) => ' '.repeat(nFloors - i - 1) + '*'.repeat(i * 2 + 1) +  ' '.repeat(nFloors - i - 1))
+}
+
+const { assert } = require("chai")
+
+describe("Tests", () => {
+  it("test", () => {
+    assert.deepEqual(towerBuilder(1), ["*"]);
+    assert.deepEqual(towerBuilder(2), [" * ","***"]);
+    assert.deepEqual(towerBuilder(3), ["  *  "," *** ","*****"]);
+  });
+});
+
+⬇️⬇️
+
+/* Example：
+[
+  '  *  ', 
+  ' *** ', 
+  '*****'
+]
+*/
