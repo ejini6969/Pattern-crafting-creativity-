d8602dfdf7eb225ec720e313e5d9883b2b5d4f29
ejini战神 <65498235+ejini6969@users.noreply.github.com>
2021-07-05 18:17:38 +0800

    Create chessboard.js

---
+++ Javascript/chessboard.js
@@ -0,0 +1,22 @@
+function board(size) {
+  return Array.from({length: size}, (_, i) => Array.from({length: size}, (_, j) => (i + j) % 2? ' ': '#').join``).join('\n')
+}
+
+const { assert } = require("chai")
+
+describe("Tests", () => {
+  it("test", () => {
+	  assert.strictEqual(board(5), "# # #\n # # \n# # #\n # # \n# # #\n");
+	  assert.strictEqual(board(11), "# # # # # #\n # # # # # \n# # # # # #\n # # # # # \n# # # # # #\n # # # # # \n# # # # # #\n # # # # # \n# # # # # #\n # # # # # \n# # # # # #\n");
+	});
+});
+
+⬇️⬇️
+
+/* Example：
+# # #
+ # #
+# # #
+ # #
+# # #
+*/
