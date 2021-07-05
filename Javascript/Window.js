a006b23cb13f9c80e473d6a68e42f1762e41011c
ejini战神 <65498235+ejini6969@users.noreply.github.com>
2021-07-05 14:16:41 +0800

    Create Window.js

---
+++ Javascript/Window.js
@@ -0,0 +1,27 @@
+function makeAWindow(num) {
+ let side = `|${'.'.repeat(num)}|${'.'.repeat(num)}|\n`.repeat(num)
+ let centre = `|${'-'.repeat(num)}+${'-'.repeat(num)}|\n`
+ return '-'.repeat(num * 2 + 3) + '\n' + side + centre + side + '-'.repeat(num * 2 + 3) + '\n'
+}
+
+const { assert } = require("chai")
+
+describe("Tests", () => {
+  it("test", () => {
+	  assert.strictEqual(makeAWindow(3), "---------\n|...|...|\n|...|...|\n|...|...|\n|---+---|\n|...|...|\n|...|...|\n|...|...|\n---------");
+  });
+});
+
+⬇️⬇️
+
+/* Example：
+---------
+|...|...|
+|...|...|
+|...|...|
+|---+---|
+|...|...|
+|...|...|
+|...|...|
+---------
+*/
