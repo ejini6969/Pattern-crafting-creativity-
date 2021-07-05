dbf2cf6a45636642b70a9e50e8740379b7c0023b
ejini战神 <65498235+ejini6969@users.noreply.github.com>
2021-07-05 18:23:05 +0800

    Update chessboard (array).js

--- Javascript/chessboard (array).js
+++ Javascript/chessboard (array).js
@@ -6,11 +6,11 @@ const { assert } = require("chai")
 
 describe("Tests", () => {
   it("test", () => {
-	  assert.strictEqual(hessBoard(1, 1),[["O"]])
-    assert.strictEqual(chessBoard(1, 2),[["O","X"]])
-    assert.strictEqual(chessBoard(2, 1),[["O"],["X"]])
-    assert.strictEqual(chessBoard(2, 2),[["O","X"],["X","O"]])
-    assert.strictEqual(chessBoard(6, 6),[['O','X','O','X','O','X'],['X','O','X','O','X','O'],['O','X','O','X','O','X'],['X','O','X','O','X','O'],['O','X','O','X','O','X'],['X','O','X','O','X','O']])
+    assert.deepEqual(hessBoard(1, 1),[["O"]])
+    assert.deepEqual(chessBoard(1, 2),[["O","X"]])
+    assert.deepEqual(chessBoard(2, 1),[["O"],["X"]])
+    assert.deepEqual(chessBoard(2, 2),[["O","X"],["X","O"]])
+    assert.deepEqual(chessBoard(6, 6),[['O','X','O','X','O','X'],['X','O','X','O','X','O'],['O','X','O','X','O','X'],['X','O','X','O','X','O'],['O','X','O','X','O','X'],['X','O','X','O','X','O']])
   });
 });
 
