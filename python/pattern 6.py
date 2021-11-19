3d6338227d6551818cb3096f828f9d5d98d8eb7d
ejini战神 <65498235+ejini6969@users.noreply.github.com>
2021-11-19 21:43:43 +0800

    Create pattern 6.py

---
+++ python/pattern 6.py
@@ -0,0 +1,25 @@
+def pattern(n):
+    return '\n'.join(str(i) * i for i in range(1, n + 1, 2))
+  
+import codewars_test as test
+
+@test.describe("Basic tests")
+def f():
+    @test.it("")
+    def f():
+        test.assert_equals(pattern(4),"1\n333")
+        test.assert_equals(pattern(1),"1")
+        test.assert_equals(pattern(5),"1\n333\n55555")
+        test.assert_equals(pattern(5),"1\n333\n55555")
+        test.assert_equals(pattern(0),"")
+        test.assert_equals(pattern(-25),"")
+
+⬇️⬇️
+
+/* Example:
+1
+333
+55555
+7777777
+999999999
+*/        
