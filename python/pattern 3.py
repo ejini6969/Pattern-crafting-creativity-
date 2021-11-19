9b42dc9b2feeb2387eae511c454883c6e27fe2f8
ejini战神 <65498235+ejini6969@users.noreply.github.com>
2021-11-19 21:40:55 +0800

    Create pattern 3.py

---
+++ python/pattern 3.py
@@ -0,0 +1,22 @@
+def pattern(n):
+    return '\n'.join(''.join(str(i) for i in range(n, j, -1)) for j in range(n - 1, -1, -1))
+  
+@test.describe("Basic tests")
+def basic():
+    @test.it("")
+    def f():
+        test.assert_equals(pattern(1),"1")
+        test.assert_equals(pattern(2),"2\n21")
+        test.assert_equals(pattern(5),"5\n54\n543\n5432\n54321")
+        test.assert_equals(pattern(0),"")
+        test.assert_equals(pattern(-25),"")
+ 
+⬇️⬇️
+
+/* Example:
+5
+54
+543
+5432
+54321
+*/
