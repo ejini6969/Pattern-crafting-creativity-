67fa738944b6de13b719a7bc2d7fafb184974a80
ejini战神 <65498235+ejini6969@users.noreply.github.com>
2021-11-19 21:37:45 +0800

    Create pattern 1.py

---
+++ python/pattern 1.py
@@ -0,0 +1,22 @@
+def pattern(n): 
+    return '\n'.join(str(i) * i for i in range(1, n + 1))
+ 
+@test.describe("Fixed tests")
+def fixed():
+    @test.it("")
+    def f():
+        test.assert_equals(pattern(1),"1")
+        test.assert_equals(pattern(2),"1\n22")
+        test.assert_equals(pattern(5),"1\n22\n333\n4444\n55555")
+        test.assert_equals(pattern(0),"")
+        test.assert_equals(pattern(-25),"")
+        
+⬇️⬇️
+
+/* Example：
+1
+22
+333
+4444
+55555
+*/
