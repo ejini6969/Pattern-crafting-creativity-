800da5160ad56e09bb150f04a213ae703e86f72a
ejini战神 <65498235+ejini6969@users.noreply.github.com>
2021-11-23 22:34:04 +0800

    Update stairs.js

--- Javascript/stairs.js
+++ Javascript/stairs.js
@@ -13,7 +13,7 @@ describe("Tests", () => {
 
 ⬇️⬇️
 
-/* Example(12)：
+/* Example (n = 12)：
                                             1 1
                                         1 2 2 1
                                     1 2 3 3 2 1
