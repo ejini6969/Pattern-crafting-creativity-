c13c20237e7f419fd21853e19d1a0178e4ab5676
ejini战神 <65498235+ejini6969@users.noreply.github.com>
2021-11-23 23:08:00 +0800

    Create Table patten.js

---
+++ Javascript/Table patten.js
@@ -0,0 +1,54 @@
+function pattern(rows,columns,str){
+ const boarder = '+---'.repeat(columns) + '+'
+ str = str.padEnd(rows * columns, ' ')
+ return `${boarder}\n` + Array.from({length: str.length / columns}, (_, i) => str.slice(i * columns, i * columns + columns)).map(x => x.replace(/./g, a => `| ${a} `) + '|').join(`\n${boarder}\n`) + `\n${boarder}`
+}
+
+describe("Basic Tests", function(){
+  it("It should works for basic tests", function(){
+
+var result=
+`+---+---+---+---+
+| H | e | l | l |
++---+---+---+---+
+| o |   | W | o |
++---+---+---+---+
+| r | l | d | ! |
++---+---+---+---+
+|   |   |   |   |
++---+---+---+---+`
+Test.assertEquals(pattern(4,4,"Hello World!") , result) 
+
+var result=
+`+---+---+---+
+| c | o | d |
++---+---+---+
+| e | w | a |
++---+---+---+
+| r | s |   |
++---+---+---+`
+Test.assertEquals(pattern(3,3,"codewars") , result) 
+
+var result=
+`+---+---+---+---+
+| N | i | c | e |
++---+---+---+---+
+|   | p | a | t |
++---+---+---+---+
+| t | e | r | n |
++---+---+---+---+`
+Test.assertEquals(pattern(3,4,"Nice pattern") , result) 
+
+var result=
+`+---+---+---+
+| N | i | c |
++---+---+---+
+| e |   | p |
++---+---+---+
+| a | t | t |
++---+---+---+
+| e | r | n |
++---+---+---+`
+Test.assertEquals(pattern(4,3,"Nice pattern") , result) 
+
+})})
