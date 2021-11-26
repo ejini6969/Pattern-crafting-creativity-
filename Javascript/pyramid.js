97f7f4d6ceb48bc492b9f78a93b04fd0abe46f67
ejini战神 <65498235+ejini6969@users.noreply.github.com>
2021-11-26 19:15:35 +0800

    Create pyramid.js

---
+++ Javascript/pyramid.js
@@ -0,0 +1,22 @@
+function pyramid(n){
+  return n == 1? '/\\\n': Array.from({length: n - 1}, (_, i) => `${' '.repeat(n - i - 1)}/${' '.repeat(i * 2)}\\`).join('\n') + `\n/${'_'.repeat(n * 2 - 2)}\\\n`
+}
+
+describe("Basic tests",() =>{
+  Test.assertEquals(pyramid(1),   "/\\\n");
+  Test.assertEquals(pyramid(2),   " /\\\n/__\\\n");
+  Test.assertEquals(pyramid(4),   "   /\\\n  /  \\\n /    \\\n/______\\\n");
+  Test.assertEquals(pyramid(6),   "     /\\\n    /  \\\n   /    \\\n  /      \\\n /        \\\n/__________\\\n");
+  Test.assertEquals(pyramid(10),  "         /\\\n        /  \\\n       /    \\\n      /      \\\n     /        \\\n    /          \\\n   /            \\\n  /              \\\n /                \\\n/__________________\\\n");
+})
+
+⬇️⬇️
+
+/* Example (n = 6)：
+     /\
+    /  \
+   /    \
+  /      \
+ /        \
+/__________\   
+*/
