16571347e289f7883e12d62d0d30af3cd011b4b6
ejini战神 <65498235+ejini6969@users.noreply.github.com>
2021-11-26 19:19:10 +0800

    Create ASCII Fun #1: X- Shape.js

---
+++ python/ASCII Fun #1: X- Shape.js
@@ -0,0 +1,15 @@
+def x(n):
+    return '\n'.join(''.join('□■'[i == j or i == n - j - 1] for j in range(n)) for i in range(n))
+ 
+test.describe("Default test cases")
+test.it("Should work for test cases")
+test.assert_equals(x(3), "■□■\n□■□\n■□■", "Should look like this: \n■□■\n□■□\n■□■")
+test.assert_equals(x(7), "■□□□□□■\n□■□□□■□\n□□■□■□□\n□□□■□□□\n□□■□■□□\n□■□□□■□\n■□□□□□■", "Should look like this: \n■□□□□□■\n□■□□□■□\n□□■□■□□\n□□□■□□□\n□□■□■□□\n□■□□□■□\n■□□□□□■")
+
+⬇️⬇️
+
+"""Example (n = 3)：
+■□■
+□■□
+■□■
+"""
