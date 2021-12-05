0624f298cf37cb83a2185610f99347e689b94abc
ejini战神 <65498235+ejini6969@users.noreply.github.com>
2021-12-05 19:17:55 +0800

    Update pyramid.js

--- Javascript/pyramid.js
+++ Javascript/pyramid.js
@@ -1,3 +1,5 @@
+// https://www.codewars.com/kata/5a1c28f9c9fc0ef2e900013b
+
 function pyramid(n){
   return n == 1? '/\\\n': Array.from({length: n - 1}, (_, i) => `${' '.repeat(n - i - 1)}/${' '.repeat(i * 2)}\\`).join('\n') + `\n/${'_'.repeat(n * 2 - 2)}\\\n`
 }
