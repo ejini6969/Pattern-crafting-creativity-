5f8ab6476367dfc40d905e640990861fe5c814a8
ejini战神 <65498235+ejini6969@users.noreply.github.com>
2021-12-05 19:03:39 +0800

    Update chessboard.js

--- Javascript/chessboard.js
+++ Javascript/chessboard.js
@@ -1,3 +1,5 @@
+// https://www.codewars.com/kata/581c894633b9fe465d0000a6
+
 function board(size) {
   return Array.from({length: size}, (_, i) => Array.from({length: size}, (_, j) => (i + j) % 2? ' ': '#').join``).join('\n')
 }
