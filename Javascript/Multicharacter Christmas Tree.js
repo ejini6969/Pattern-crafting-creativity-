a4e55f6e5777f5f0d9758571b5a71ddf36478e39
ejini战神 <65498235+ejini6969@users.noreply.github.com>
2021-12-05 16:24:29 +0800

    Update Multicharacter Christmas Tree.js

--- Javascript/Multicharacter Christmas Tree.js
+++ Javascript/Multicharacter Christmas Tree.js
@@ -1,3 +1,5 @@
+// https://www.codewars.com/kata/5a405ba4e1ce0e1d7800012e
+
 function customChristmasTree(chars,n){
   chars = chars.padEnd(n * (n + 1) / 2, chars)
   let tree = Array.from({length: n}, (_, i) => `${" ".repeat(n - i - 1)}${chars.split("").slice(i * (i + 1) / 2 | 0, (i + 1) * (i + 2) / 2 | 0).join(" ")}`).join`\n` 
