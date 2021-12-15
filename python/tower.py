7f8465864a276cbb9dc29a7cacb658b3497e7441
ejini战神 <65498235+ejini6969@users.noreply.github.com>
2021-12-15 15:36:13 +0800

    Update tower.py

--- python/tower.py
+++ python/tower.py
@@ -1,3 +1,5 @@
+# https://www.codewars.com/kata/576757b1df89ecf5bd00073b
+
 def tower_builder(n_floors):
     return [' ' * (n_floors - i - 1) + '*' * (i * 2 + 1) + ' ' * (n_floors - i - 1) for i in range(n_floors)]
   
