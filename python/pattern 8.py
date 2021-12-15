17f593deafa9566848292d698dea5cd9b5eb7528
ejini战神 <65498235+ejini6969@users.noreply.github.com>
2021-12-15 15:30:55 +0800

    Update pattern 8.py

--- python/pattern 8.py
+++ python/pattern 8.py
@@ -1,3 +1,5 @@
+# https://www.codewars.com/kata/5575ff8c4d9c98bc96000042
+
 def pattern(n):
     return "\n".join([" " * (n - i) + ''.join(str(j % 10) for j in range(1, i)) + str(i % 10) + ''.join(str(k % 10) for k in range(i - 1, 0, -1)) + " " * (n - i) for i in range(1, n + 1)])
   
