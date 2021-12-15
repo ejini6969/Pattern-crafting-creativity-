f2df3503775994a34a9d99d7681c6321d7e654fd
ejini战神 <65498235+ejini6969@users.noreply.github.com>
2021-12-15 15:30:32 +0800

    Update pattern 7.py

--- python/pattern 7.py
+++ python/pattern 7.py
@@ -1,3 +1,5 @@
+# https://www.codewars.com/kata/557592fcdfc2220bed000042
+
 def pattern(n):
     a = [str(i) for i in range(1, n + 1)]
     return '\n'.join(''.join(a[i:] + a[:i]) for i in range(n))
