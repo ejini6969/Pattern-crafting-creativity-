52895fcca2f945a25efdf7b5deedaa774f7b5655
ejini战神 <65498235+ejini6969@users.noreply.github.com>
2021-12-15 15:34:18 +0800

    Update pyramid.py

--- python/pyramid.py
+++ python/pyramid.py
@@ -1,3 +1,5 @@
+# https://www.codewars.com/kata/5a1c28f9c9fc0ef2e900013b
+
 def pyramid(n):
     return "/\\\n" if n == 1 else "\n".join(f"{' ' * (n - i - 1)}/{' ' * (i * 2)}\\" for i in range(n - 1)) + f"\n/{'_' * (n * 2 - 2)}\\\n"
   
